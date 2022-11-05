1332ac7d799d1b78dccbe264065428730c9437b8
manudiiez <manudiiez123@gmail.com>
2022-11-05 13:32:43 -0300

    desafio 4

--- apiRestFul/controllers/controllerProductos.js
+++ apiRestFul/controllers/controllerProductos.js
@@ -1,49 +1,58 @@
 const { randomUUID } = require('crypto');
 
-const productos = []
+class ContenedorArchivo {
 
-const getAll = (req, res) => {
-    res.status(200).json({data: productos})
-}
-const getById = (req, res) => {
-    const id = req.params.id
-    const producto = productos.find(item => item.id === id);
-    if(!producto){
-        res.status(404).json({menssage: `No se encontro ningun producto con el id: ${id}`})
-    }
-    res.status(200).json({data: producto})
-}
-const save = (req, res) => {
-    const id = randomUUID();
-    const newProducto = {
-        ...req.body,
-        id: id
-    }
-    productos.push(newProducto)
-    res.status(201).json({data: newProducto})
-}
-const updateById = (req, res) => {
-    const id = req.params.id
-    const productoIndex = productos.findIndex(item => item.id === id)
-    if(productoIndex === -1){
-        res.status(404).json({menssage: `No se encontro ningun producto con el id: ${id}`})
+    constructor() {
+        this.productos = []
     }
 
-    productos[productoIndex] = {...req.body, id: id};
-    res.status(200).json({message: `Producto ${id} actualizado`})
-}
-const deleteById = (req, res) => {
-    const id = req.params.id
-    const productoIndex = productos.findIndex(item => item.id === id)
-    if(productoIndex === -1){
-        res.status(404).json({menssage: `No se encontro ningun producto con el id: ${id}`})
+
+    getAll = (req, res) => {
+        res.status(200).json({data: this.productos})
     }
-    productos.splice(productoIndex, 1)
-    res.status(200).json({message: `Producto ${id} eliminado`})
+
+    save = (req, res) => {
+        const id = randomUUID();
+        const newProducto = {
+            ...req.body,
+            id: id
+        }
+        this.productos.push(newProducto)
+        res.status(201).json({data: newProducto})
+    }
+
+    getById = (req, res) => {
+        const id = req.params.id
+        const producto = this.productos.find(item => item.id === id);
+        if(!producto){
+            res.status(404).json({error: `No se encontro ningun producto con el id: ${id}`})
+        }
+        res.status(200).json({data: producto})
+    }
+
+    updateById = (req, res) => {
+        const id = req.params.id
+        const productoIndex = this.productos.findIndex(item => item.id === id)
+        if(productoIndex === -1){
+            res.status(404).json({error: `No se encontro ningun producto con el id: ${id}`})
+        }
+    
+        this.productos[productoIndex] = {...req.body, id: id};
+        res.status(200).json({message: `Producto ${id} actualizado`})
+    }
+
+    deleteById = (req, res) => {
+        const id = req.params.id
+        const productoIndex = this.productos.findIndex(item => item.id === id)
+        if(productoIndex === -1){
+            res.status(404).json({error: `No se encontro ningun producto con el id: ${id}`})
+        }
+        this.productos.splice(productoIndex, 1)
+        res.status(200).json({message: `Producto ${id} eliminado`})
+    }
+
 }
 
-exports.getAll = getAll;
-exports.getById = getById;
-exports.save = save;
-exports.updateById = updateById;
-exports.deleteById = deleteById;
+const contenedor = new ContenedorArchivo()
+
+exports.contenedor = contenedor;
--- apiRestFul/servidor.js
+++ apiRestFul/servidor.js
@@ -4,8 +4,12 @@ const express = require('express')
 
 
 const app = express()
+
+
 app.use(express.json())
 // app.use(express.urlencoded({ extended: true }))
+app.use('/public', express.static('archivos'))
+app.use('/views', express.static('views'))
 
 app.use('/api/productos', routerProductos)
 
